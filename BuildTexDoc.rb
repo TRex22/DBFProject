9247693505b083797aa7bf5de92d74577cc3d07b
jonah-hooper <jonah.graham.hooper@gmail.com>
2014-03-23 20:01:13 +0200

    Made skeleton document. Made project skeleton generation file. removed spaces from filenames

---
+++ BuildTexDoc.rb
@@ -0,0 +1,35 @@
+print "\\documentclass[a4paper,10pt]{article}
+
+\\usepackage{listings}
+\\usepackage[pdftex]{graphicx}
+\\title{Database fundementals final project}
+\\author{Name and student number}
+\\date{23 March 2014}
+
+\\begin{document}
+\\maketitle"
+Dir.new("Queries").sort.each do |dir|
+  if dir != "." && dir != ".."
+    contents_of_dir = Dir.new("Queries/#{dir}").map {|m| m if m != "." && m != ".."}
+    contents_of_dir.delete(nil)
+    contents_of_dir = contents_of_dir.map do |file|
+      unless (file.include? "PNG") || (file.include? "jpg")
+        source_file = File.read "Queries/#{dir}/#{file}"
+        "\\lstset{
+            language=SQL,
+            breaklines=true
+            }
+        \\begin{lstlisting}[frame=single]
+        #{source_file}
+        \\end{lstlisting}"
+      else
+          "\\includegraphics{Queries/#{dir}/#{file}}"
+      end
+
+    end
+
+    print "\\section {#{dir.sub! "_", " "}}\n" 
+    print "#{contents_of_dir.join "\n"}\n"
+  end
+end
+print "\\end{document}"
